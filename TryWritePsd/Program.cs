2b18c3eeb8b66c2b705cd8423ccc9a89a667624e
nooblesone <nooblesone@163.com>
2025-12-02 22:06:01 +0800

    初步完成功能验证

--- TryWritePsd/Program.cs
+++ TryWritePsd/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Buffers.Binary;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
 using System.Runtime.Serialization.Formatters;
 using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 if (File.Exists("./test_converted.psd"))
     File.Delete("./test_converted.psd");
 using FileStream psfs = new("./test.psd", FileMode.Open);
@@ -56,7 +61,14 @@ var by4 = br.ReadBytes(layerandMaskInformationSectionHeadLength);
 layerandMaskInformationSectionLength = BinaryPrimitives.ReadInt32BigEndian(by4);
 bw.Write(by4);
 var layerArea = br.ReadBytes(layerandMaskInformationSectionLength);
-layerArea = Funnnn(layerArea, 0, 0);
+var layers = GetLayers(layerArea);
+
+foreach (var item in layers)
+{
+    item.Clipping = 1;
+}
+
+
 bw.Write(layerArea);
 
 
@@ -87,14 +99,72 @@ Console.WriteLine($"图层数： {BinaryPrimitives.ReadInt16BigEndian(ssss)}");
 
 
 
-static byte[] Funnnn(byte[] layerArea, int start, int end)
+
+
+static List<PsLayer> GetLayers(byte[] layerArea)
 {
-    var sadf = layerArea[22..24];
-    var channelsC = BinaryPrimitives.ReadInt16BigEndian(sadf);
-    var index = 6 + 16 + 2 + 6 * channelsC + 4 + 4 + 1;
-    layerArea[index] = 1;
+
+    //var layerAndMaskInformationLength = BinaryPrimitives.ReadInt32BigEndian(layerArea.AsSpan()[0..4]);
+
+    var index = 0;
+
+    var layerInfoLength = BinaryPrimitives.ReadInt32BigEndian(layerArea.AsSpan()[index..(index + 4)]);
+    index += 4;
+    var layerCount = BinaryPrimitives.ReadInt16BigEndian(layerArea.AsSpan()[index..(index + 2)]);
+    index += 2;
+
+    List<PsLayer> layers = new(layerCount);
+    for (var i = 0; i < layerCount; i++)
+    {
+        index += 16;
+        var channelsCount = BinaryPrimitives.ReadInt16BigEndian(layerArea.AsSpan()[index..(index + 2)]);
+        index += 2;
+        index = index + 6 * channelsCount + 4 + 4 + 1;
+        var clippingIndex = index;
+        var clipping = layerArea[clippingIndex];
+        index++;
+        index++;
+        index++;
+        var extraDataFieldLength = BinaryPrimitives.ReadInt32BigEndian(layerArea.AsSpan()[(index)..(index + 4)]);
+        index += 4;
+        var layerMaskDataLength = BinaryPrimitives.ReadInt32BigEndian(layerArea.AsSpan()[(index)..(index + 4)]);
+        var layerBlendingRangesLength = BinaryPrimitives.ReadInt32BigEndian(layerArea.AsSpan()[(index + layerMaskDataLength)..(index + layerMaskDataLength + 4)]);
+        var nameIndex = index + 4 + layerMaskDataLength + 4 + layerBlendingRangesLength;
+        var nameLength = layerArea[nameIndex];
+        var name = Encoding.Latin1.GetString(layerArea.AsSpan()[(nameIndex + 1)..(nameIndex + 1 + nameLength)]);
+        var layer = new PsLayer
+        {
+            LayerBytes = layerArea,
+            ClippingIndex = clippingIndex,
+            Clipping = clipping,
+            Name = name,
+        };
+        Console.WriteLine(layer.Name);
+        layers.Add(layer);
+        index += extraDataFieldLength;
+    }
+
+    return layers;
+
+
+}
 
 
 
-    return layerArea;
+class PsLayer
+{
+    public required byte[] LayerBytes { get; init; }
+    public required int ClippingIndex { get; init; }
+    public required string Name { get; init; }
+    /// <summary>
+    /// Clipping: 0 = base, 1 = non-base
+    /// </summary>
+    public required byte Clipping
+    {
+        get; set
+        {
+            LayerBytes[ClippingIndex] = value;
+            field = value;
+        }
+    }
 }
